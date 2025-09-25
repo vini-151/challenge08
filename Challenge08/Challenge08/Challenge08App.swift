3959869e3051f5e28f67e486a2466ed3e25b8b6f
Vini <ovini.15515@gmail.com>
2025-09-25 14:57:34 -0300

    projeto do challenge

---
+++ Challenge08/Challenge08/Challenge08App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge08App.swift
+//  Challenge08
+//
+//  Created by Vini Oliveira  on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
