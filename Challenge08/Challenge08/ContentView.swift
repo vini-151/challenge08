83cd76dd52d012f506b7d5dc69d63a523f0d3462
Vini <ovini.15515@gmail.com>
2025-09-25 16:53:03 -0300

    ïnput de texto e analise com o modelo

--- Challenge08/Challenge08/ContentView.swift
+++ Challenge08/Challenge08/ContentView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
+import CoreML
+import NLModel
+
 
 struct ContentView: View {
+    
+    @State var output: String = ""
+    @State var inputText: String = ""
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField(
+                "input",
+                text: $inputText
+            )
+            .textFieldStyle(.roundedBorder)
+            .onSubmit {
+                output = PredictEmail(input: inputText).testarModel()!.label
+            }
+            Text(output)
+                .foregroundStyle(output == "positivo" ? .green : .red)
         }
         .padding()
     }
