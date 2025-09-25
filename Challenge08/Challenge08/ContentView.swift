b73be0399977a86b44d662807e8f01c47a8c9f6e
Vini <ovini.15515@gmail.com>
2025-09-25 17:06:23 -0300

    ajustando para classe

--- Challenge08/Challenge08/ContentView.swift
+++ Challenge08/Challenge08/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     @State var output: String = ""
     @State var inputText: String = ""
-
+    var predict = PredictEmail()
     
     var body: some View {
         VStack {
@@ -24,7 +24,9 @@ struct ContentView: View {
             )
             .textFieldStyle(.roundedBorder)
             .onSubmit {
-                output = PredictEmail(input: inputText).testarModel()!.label
+                if let output = predict.testarModel(input: inputText) {
+                    self.output = output.label
+                }
             }
             Text(output)
                 .foregroundStyle(output == "positivo" ? .green : .red)
