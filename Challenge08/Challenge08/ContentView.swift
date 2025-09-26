254082c05d83f4402e41ad0984ad8cb23cdb0443
Vini <ovini.15515@gmail.com>
2025-09-26 16:51:40 -0300

    adequando o projeto ao package atualizado

--- Challenge08/Challenge08/ContentView.swift
+++ Challenge08/Challenge08/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     @State var output: String = ""
     @State var inputText: String = ""
-    var predict = PredictEmail()
+    var predict = PredictText()
     
     var body: some View {
         VStack {
@@ -24,8 +24,8 @@ struct ContentView: View {
             )
             .textFieldStyle(.roundedBorder)
             .onSubmit {
-                if let output = predict.testarModel(input: inputText) {
-                    self.output = output.label
+                if let output = predict.predictModel(input: inputText) {
+                    self.output = output
                 }
             }
             Text(output)
