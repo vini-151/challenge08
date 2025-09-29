5a70f337449ea715e3cdde66d7e7c8e76c0f5f69
Vini <ovini.15515@gmail.com>
2025-09-29 17:15:31 -0300

    consumindo o pacote da camera

--- Challenge08/Challenge08/ContentView.swift
+++ Challenge08/Challenge08/ContentView.swift
@@ -8,30 +8,56 @@
 import SwiftUI
 import CoreML
 import NLModel
+import HandGesturesClassifier
 
 
 struct ContentView: View {
     
     @State var output: String = ""
     @State var inputText: String = ""
+    
+    @State private var gesture: HandPoses = .background
+    @State private var isCameraHidden: Bool = false
+    
     var predict = PredictText()
     
     var body: some View {
+            
+
+            
         VStack {
+            ARViewContainer(
+                gesture: $gesture,
+                cameraFrame: CGRect(x: 0, y: 0, width: 100, height: 100),
+                isCameraHidden: $isCameraHidden
+            )
+            
             TextField(
                 "input",
                 text: $inputText
             )
             .textFieldStyle(.roundedBorder)
             .onSubmit {
-                if let output = predict.predictModel(input: inputText) {
-                    self.output = output
-                }
             }
             Text(output)
-                .foregroundStyle(output == "positivo" ? .green : .red)
         }
         .padding()
+        .onChange(of: gesture) { _, newValue in
+            if gesture.rawValue == "aberta"{
+                if let output = predict.predictModel(input: inputText) {
+                    self.output = output
+                    print(self.output)
+                }
+            }else if gesture.rawValue == "fechada"{
+                output = ""
+                inputText = ""
+            }
+            
+        }
+        .padding()
+            
+        
+
     }
 }
 
